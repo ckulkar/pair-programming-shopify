ccefb54bc53ee7842334e2f7e3748a0df82b362a
Chaitanya <ckulkar@gmail.com>
2020-05-15 13:31:42 -0700

    step 2 - added javadoc

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -6,34 +6,10 @@ class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
-    /*
-    - All items have a SellIn value which denotes the number of days we have to sell the item
-    - All items have a Quality value which denotes how valuable the item is
-    - At the end of each day our system lowers both values for every item
     
-    Pretty simple, right? Well this is where it gets interesting:
-
-    - Once the sell by date has passed, Quality degrades twice as fast
-    - The Quality of an item is never negative
-    - "Aged Brie" actually increases in Quality the older it gets
-    - The Quality of an item is never more than 50
-    - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-    - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-    Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-    Quality drops to 0 after the concert
-    
-    We have recently signed a supplier of conjured items. This requires an update to our system:
-
-    - "Conjured" items degrade in Quality twice as fast as normal items
+    /**
+     * Update quality for inventory
      */
-    
-    /*
-     * TODO
-     * 1. Stop decrementing sellIn at 0
-     * 2. 
-     */
-    
     public void updateQuality() {
         if (validItems()) {
             for (Item item : items) {
@@ -85,6 +61,10 @@ class GildedRose {
         }
     }
     
+    /**
+     * Ensure that all the items have all non-null, non-zero fields
+     * @return true if the items are valid, false otherwise
+     */
     private boolean validItems() {
         if (items != null && items.length > 0) {
             for (Item item : items) {
@@ -96,6 +76,11 @@ class GildedRose {
         return true;
     }
     
+    /**
+     * Ensure that an item has non-null, non-zero-length fields
+     * @param item
+     * @return true if the item is valid, false otherwise
+     */
     private boolean validItem(Item item) {
         return (item != null && 
                 item.name != null && item.name.length() > 0);
@@ -115,7 +100,11 @@ class GildedRose {
     	}
     }
     
-    public void updateQuality1() {
+    /**
+     * @deprecated for poor readability and maintainability. Use as a reference for business logic
+     * and ensure that tests pass using this if needed. 
+     */
+    public void updateQualityDeprecated() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
